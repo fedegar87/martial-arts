b048d92e632f8fb96fe08daf102a3fe7127dd663
fedegar87 <168815387+fedegar87@users.noreply.github.com>
2026-05-01 17:48:10 +0200

    feat(hub): aggiunge HubBackground (cavallo watermark)

---
+++ skill-practice/src/components/hub/HubBackground.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable @next/next/no-img-element */
+
+export function HubBackground() {
+  return (
+    <img
+      src="/landing/cavallo-fuoco.svg"
+      alt=""
+      aria-hidden="true"
+      className="pointer-events-none fixed bottom-0 right-0 z-0 w-[70vw] max-w-2xl opacity-[0.05]"
+    />
+  );
+}
