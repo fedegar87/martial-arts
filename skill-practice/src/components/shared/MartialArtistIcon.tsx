76c28da0b1c711fdc1ac381a68a87fa97ff8353c
fedegar87 <168815387+fedegar87@users.noreply.github.com>
2026-05-04 23:14:11 +0200

    Improve martial artist profile icon

--- skill-practice/src/components/shared/MartialArtistIcon.tsx
+++ skill-practice/src/components/shared/MartialArtistIcon.tsx
@@ -14,12 +14,14 @@ const MartialArtistIconComponent = forwardRef<SVGSVGElement, LucideProps>(
       className={className}
       {...props}
     >
-      <circle cx="11" cy="4.5" r="2" />
-      <path d="m10 7-2 3.5 3.5 2 3.5-3" />
-      <path d="m8 10.5-4-1" />
-      <path d="m11.5 12.5-3 6.5" />
-      <path d="m8.5 19-3 1.5" />
-      <path d="m11.5 12.5 5 1.5 3.5-2.5" />
+      <circle cx="12.5" cy="4.5" r="2" />
+      <path d="M11.5 7 9 10.8l3.5 2.6 3-3.8" />
+      <path d="M9.2 10.7 5 9" />
+      <path d="M15.2 9.8 19 7.6" />
+      <path d="M12.4 13.4 9 20" />
+      <path d="M9 20H5.5" />
+      <path d="M12.8 13.4 17.2 16" />
+      <path d="M17.2 16 21 14.5" />
     </svg>
   ),
 );
